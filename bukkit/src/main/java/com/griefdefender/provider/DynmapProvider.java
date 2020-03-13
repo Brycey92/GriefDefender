90c88f2708a32a72b48e20e1613a44e0919a5f40
Bryce Browner <Brycey92@users.noreply.github.com>
2020-03-12 22:09:55 -0400

    Revert "Fixed null check running before Dynmap API fully loads (#137)"
    This reverts commit 18ba7ea3d707a41ee7b66e1c0a14b95c3c415f2e.

--- bukkit/src/main/java/com/griefdefender/provider/DynmapProvider.java
+++ bukkit/src/main/java/com/griefdefender/provider/DynmapProvider.java
@@ -81,6 +81,11 @@ public class DynmapProvider {
                 activate();
             }
         });
+        if (this.markerapi == null) {
+            this.logger.severe("Error loading Dynmap Provider! Could not locate Marker API.");
+            return;
+        }
+        GriefDefender.getEventManager().register(this);
     }
 
     private Map<String, AreaMarker> areaMarkers = new HashMap<String, AreaMarker>();
@@ -273,7 +278,6 @@ public class DynmapProvider {
     private void activate() {
         this.markerapi = this.dynmap.getMarkerAPI();
         if (this.markerapi == null) {
-            this.logger.severe("Error loading Dynmap Provider! Could not locate Marker API.");
             return;
         }
         if (this.reload) {
@@ -307,7 +311,6 @@ public class DynmapProvider {
         this.set.setHideByDefault(this.cfg.layerHideByDefault);
 
         new GriefDefenderUpdate(40L);
-        GriefDefender.getEventManager().register(this);
         this.logger.info("Dynmap provider is activated");
     }
 
--- sponge/src/main/java/com/griefdefender/provider/DynmapProvider.java
+++ sponge/src/main/java/com/griefdefender/provider/DynmapProvider.java
@@ -83,6 +83,11 @@ public class DynmapProvider {
                 activate();
             }
         });
+        if (this.markerapi == null) {
+            this.logger.error("Error loading Dynmap Provider! Could not locate Marker API.");
+            return;
+        }
+        GriefDefender.getEventManager().register(this);
     }
 
     private Map<String, AreaMarker> areaMarkers = new HashMap<String, AreaMarker>();
@@ -275,7 +280,6 @@ public class DynmapProvider {
     private void activate() {
         this.markerapi = this.dynmap.getMarkerAPI();
         if (this.markerapi == null) {
-            this.logger.error("Error loading Dynmap Provider! Could not locate Marker API.");
             return;
         }
         if (this.reload) {
@@ -309,7 +313,6 @@ public class DynmapProvider {
         this.set.setHideByDefault(this.cfg.layerHideByDefault);
 
         new GriefDefenderUpdate(40L);
-        GriefDefender.getEventManager().register(this);
         this.logger.info("Dynmap provider is activated");
     }
 
